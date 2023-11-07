1363f01e763f2c497abf894ee9886979ed2bb899
linz <linz.webdev@gmail.com>
2023-11-07 16:50:52 -0400

    Error de backpath

--- src/components/layout/content/DescripcionDeVista.tsx
+++ src/components/layout/content/DescripcionDeVista.tsx
@@ -4,7 +4,7 @@ import { useTheme } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import ArrowBackRounded from '@mui/icons-material/ArrowBackRounded';
 import { TypographyCustom } from '../../custom/TypographyCustom';
-import { useNavigate } from 'react-router-dom';
+import { To, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import HomeRounded from '@mui/icons-material/HomeRounded';
 import { AuthContext } from '../../../context/auth';
@@ -12,7 +12,7 @@ import { AuthContext } from '../../../context/auth';
 type Props = {
     title: string;
     description: string;
-    backPath?: string;
+    backPath?: string | To;
 }
 
 export const DescripcionDeVista = (props: Props) => {
@@ -22,7 +22,7 @@ export const DescripcionDeVista = (props: Props) => {
     const { authState } = useContext(AuthContext)
     return (
         <Box sx={styles.nameContainer}>
-            <Button size='small' onClick={() => router(authState.role?.description === 'Cliente' ? '/dashboard' : '/admin/dashboard')} variant="text" sx={{
+            <Button size='small' onClick={() => router(authState.role?.description === 'Master' ? '/admin/dashboard' : '/dashboard')} variant="text" sx={{
                 background: 'rgba(100,100,100,0.1)', textTransform: 'none', borderRadius: '10em', color: 'text.secondary', mb: 2, '&:hover': {
                     background: authState.color,
                     color: theme.palette.getContrastText(authState.color)
@@ -34,9 +34,9 @@ export const DescripcionDeVista = (props: Props) => {
             </Button>
 
             <Box sx={{ display: 'flex', alignItems: 'center', flexFlow: 'row nowrap' }}>
-                <IconButton onClick={() => router((props.backPath ? props.backPath : "-1"))}>
-                    <ArrowBackRounded />
-                </IconButton>
+            <IconButton onClick={() => router(props.backPath ? props.backPath : -1 as To)}>
+                <ArrowBackRounded />
+            </IconButton>
                 <TypographyCustom variant='h4' fontWeight={'bold'}>
                     {props.title}
                 </TypographyCustom>
--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -1,11 +1,25 @@
 import { FC } from 'react'
 import { Layout } from '../../components/layout'
 import { DescripcionDeVista } from '../../components/layout/content'
+import { Option } from '../../interfaces'
+import SportsBaseball from '@mui/icons-material/SportsBaseball'
+import Diversity2 from '@mui/icons-material/Diversity2'
+import PriceCheck from '@mui/icons-material/PriceCheck'
+import Groups from '@mui/icons-material/Groups'
+import { green, blue, orange, pink } from '@mui/material/colors'
+import { OptionsList } from '../../components/layout/options'
 
 export const AdminDashboard: FC = () => {
+    const clientOptions: Option[] = [
+        { text: 'Jugadores', icon: <SportsBaseball/>, color: green[400], path: '/admin/players' },
+        { text: 'Ligas', icon: <Diversity2/>, color: orange[400], path: '/admin/leagues' },
+        { text: 'Equipos', icon: <Groups/>, color: pink[400], path: '/admin/teams' },
+        { text: 'Pagos', icon: <PriceCheck/>, color: blue[400], path: '/admin/payments' },
+    ]
     return (
         <Layout>
             <DescripcionDeVista title={'Pagina principal'} description={'Bienvenido, elige una de las opciones para interactuar con el sistema ¿Que deseas hacer?'} />
+            <OptionsList options={clientOptions} breakpoints={{ xs: 6, sm: 6, md: 3, lg: 3 }} />
         </Layout>
     )
 }
